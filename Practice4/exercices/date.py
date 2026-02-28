34e1a0d6ee7a321d8c0e6427dd3f5cf5bbcc324a
BagdatAitmuratov <bagaland12345@gmail.com>
2026-02-28 09:12:43 +0500

    pp2

--- Practice4/exercices/date.py
+++ Practice4/exercices/date.py
@@ -1,21 +1,21 @@
 from datetime import datetime, timedelta
 
-# 5 күн азайту
+
 current_date = datetime.now()
 five_days_ago = current_date - timedelta(days=5)
-print(f"5 күн бұрын: {five_days_ago.strftime('%Y-%m-%d')}")
+print(f"After 5 days: {five_days_ago.strftime('%Y-%m-%d')}")
 
-#Кеше бүгін ертең
+
 today = datetime.now().date()
 yesterday = today - timedelta(days=1)
 tomorrow = today + timedelta(days=1)
-print(f"Кеше: {yesterday} | Бүгін: {today} | Ертең: {tomorrow}")
+print(f"Yesterday: {yesterday} | Today: {today} | Tommarrow: {tomorrow}")
 
-#Микросекундтарды алып тастау
+
 now_no_ms = datetime.now().replace(microsecond=0)
-print(f"Микросекундсыз: {now_no_ms}")
+print(f"without microsecoud: {now_no_ms}")
 
-#Екі күннің айырмашылығы секундпен
+
 d1 = datetime(2026, 2, 27, 12, 0, 0)
 d2 = datetime(2026, 2, 28, 12, 0, 0)
-print(f"Айырмашылық: {abs((d2 - d1).total_seconds())} секунд")
+print(f"Diference: {abs((d2 - d1).total_seconds())} sec")
--- Practice4/exercices/generator.py
+++ Practice4/exercices/generator.py
@@ -1,23 +1,24 @@
-# 1. Квадраттар генераторы
+
 def square_gen(N):
     for i in range(N + 1):
         yield i ** 2
 
-# 2. Жұп сандарды үтірмен шығару
+
 def even_gen(n):
     for i in range(n + 1):
         if i % 2 == 0:
             yield str(i)
 
-n_val = int(input("Сан енгізіңіз (n): "))
-print("Жұп сандар:", ", ".join(even_gen(n_val)))
+n_val = int(input())
+print(", ".join(even_gen(n_val)))
+
 
 def div_3_4(n):
     for i in range(n + 1):
         if i % 3 == 0 and i % 4 == 0:
             yield i
 
-
+#квадрат
 def squares(a, b):
     for i in range(a, b + 1):
         yield i ** 2
