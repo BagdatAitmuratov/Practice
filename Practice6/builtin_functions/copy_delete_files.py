3e19203701a309d9391b7b3176d34f2f9f40c3ee
BagdatAitmuratov <bagaland12345@gmail.com>
2026-03-26 23:56:14 +0500

    Add Practice6 - file handling and built-in functions examples

--- Practice6/builtin_functions/copy_delete_files.py
+++ Practice6/builtin_functions/copy_delete_files.py
@@ -1,10 +1,15 @@
 import shutil
 import os
-
-with open("maatynn.txt", "w") as f:
-    f.write("test content")
-
-shutil.copy("source.txt", "destination.txt")
-
-if os.path.exists("destination.txt"):
-    os.remove("destination.txt")
+with open("bag.txt","w")as file:
+    file.write("hello my name is BAGDAT!!I'm from Aral")
+shutil.copy("bag.txt","copy_bag.txt")
+# болек файлға лактр
+# |
+# |
+# V
+if os.path.exists("copy_bag.txt"):
+    os.remove("copy_bag.txt")
+    print("File oshirildi")
+else:
+    print("жайлы жок табылмады")
+# сол кезде ошеди
