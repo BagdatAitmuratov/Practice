daa02faf4eff00aca175b66e84857b72cba3fdf0
BagdatAitmuratov <bagaland12345@gmail.com>
2026-03-28 11:45:57 +0500

    pratice7 and phonebook with PSQL

--- Practice7/phonebook.py
+++ Practice7/phonebook.py
@@ -1,10 +1,17 @@
 import psycopg2
 import csv
 from config import parametrs
-#1.1
+
+def get_conn():
+    return psycopg2.connect(
+        host=parametrs['host'].strip(),
+        database=parametrs['database'].strip(),
+        user=parametrs['user'].strip(),
+        password=parametrs['password'].strip()
+    )
+
 def crate_table():
-    conn_str = f"host='{parametrs['host']}' dbname='{parametrs['database']}' user='{parametrs['user']}' password='{parametrs['password']}'"
-    with psycopg2.connect(**parametrs) as conn:
+    with get_conn() as conn:
         with conn.cursor() as f:
             f.execute('''
                 CREATE TABLE IF NOT EXISTS phonebook (
@@ -14,64 +21,62 @@ def crate_table():
                 );
             ''')
         conn.commit()
-#1.2
+
 def insetr_csv(file):
-    with psycopg2.connect(**parametrs) as conn:
+    with get_conn() as conn:
         with conn.cursor() as c:
             with open(file, 'r', encoding='utf-8') as f:
                 r = csv.reader(f)
                 for row in r:
                     c.execute("INSERT INTO phonebook (name, phone) VALUES (%s, %s)", row)
         conn.commit()
-#1.3
+
 def add_contact(name, phone):
-    with psycopg2.connect(**parametrs) as conn:
+    with get_conn() as conn:
         with conn.cursor() as throw:
             throw.execute("INSERT INTO phonebook (name, phone) VALUES (%s, %s)", (name, phone))
         conn.commit()
-#1.4
+
 def update_contact(name, new_phone):
-    with psycopg2.connect(**parametrs) as conn:
+    with get_conn() as conn:
         with conn.cursor() as update:
             update.execute("UPDATE phonebook SET phone=%s WHERE name=%s", (new_phone, name))
         conn.commit()
-#1.5
+
 def contact_find(soz):
-    with psycopg2.connect(**parametrs) as conn:
+    with get_conn() as conn:
         with conn.cursor() as throw:
             throw.execute("SELECT * FROM phonebook WHERE name LIKE %s OR phone LIKE %s", (f'%{soz}%', f'{soz}%'))
             res = throw.fetchall()
             if res:
                 for row in res: print(row)
-            else:
-                pass
-#1.6
+
 def contact_del(name):
-    with psycopg2.connect(**parametrs) as conn:
+    with get_conn() as conn:
         with conn.cursor() as throw:
             throw.execute("DELETE FROM phonebook WHERE name = %s", (name,))
         conn.commit()
+def see_all():
+    with get_conn() as conn:
+        with conn.cursor()as throw:
+            throw.execute("select*from phonebook")
+            rows=throw.fetchall()
+            for row in rows:
+                print(row)
+    conn.commit()
+
 def menu():
-    crate_table();
+    crate_table()
     while True:
-        print("\n--- PhoneBook menu ---")
-        print("1: CSV\n2: Қосу \n3: Жаңарту \n4: Іздеу \n5: Өшіру \n0: Шығу")
-        sol = input("Tangdau: ")
+        print("\n1: CSV | 2: Add | 3: Update | 4: Find | 5: Delete |6: See al persons | 0: Exit")
+        sol = input("Choice: ")
+        if sol == '1': insetr_csv('contacts.csv')
+        elif sol == '2': add_contact(input("Name: "), input("Phone: "))
+        elif sol == '3': update_contact(input("Name: "), input("New Phone: "))
+        elif sol == '4': contact_find(input("Search: "))
+        elif sol == '5': contact_del(input("Name: "))
+        elif sol == '6': see_all()
+        elif sol == '0': break
 
-        if sol == '1':
-            insetr_csv('contacts.csv')
-        elif sol == '2':
-            add_contact(input("Аты: "), input("Телефон: "))
-        elif sol == '3':
-            update_contact(input("Кімді өзгертеміз (аты): "), input("Жаңа нөмір: "))
-        elif sol == '4':
-            contact_find(input("Іздеу үшін ат немесе нөмір жазыңыз: "))
-        elif sol == '5':
-            contact_del(input("Кімді өшіреміз: "))
-        elif sol == '0':
-            print("Сау болыңыз!")
-            break
-        else:
-            print("Қате таңдау, қайта көріңіз.")
-
+if __name__ == "__main__":
     menu()
